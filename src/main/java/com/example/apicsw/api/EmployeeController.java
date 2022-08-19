d275d7a23028198fcd7a33e57aaa2c8a11776704
Nguyen Van Duc <ducbe2k2@gmail.com>
2022-08-19 20:35:37 +0700

    API CSW CORS

--- src/main/java/com/example/apicsw/api/EmployeeController.java
+++ src/main/java/com/example/apicsw/api/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "https://client-csw.vercel.app/")
+@CrossOrigin(origins = {"https://client-csw.vercel.app/","localhost:3000"})
 @RestController
 @RequestMapping("/api/employee")
 @RequiredArgsConstructor
