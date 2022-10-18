54d52d8bfb04695e63651e2992f6840e4bc1ef17
dducnv0712 <ducbe2k2@gmail.com>
2022-10-18 09:59:34 +0700

    DATA BASE CONFIG

--- src/main/java/com/example/apicsw/api/EmployeeController.java
+++ src/main/java/com/example/apicsw/api/EmployeeController.java
@@ -9,35 +9,35 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = {"https://client-csw.vercel.app/","http://localhost:3000"})
 @RestController
-@RequestMapping("/api/employee")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
     public ResponseEntity<?> findAll() {
         return ResponseEntity.ok(employeeService.findAll());
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> findById(@PathVariable Long id) {
         return ResponseEntity.ok(employeeService.findById(id));
     }
 
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @RequestMapping(value = "/employee", method = RequestMethod.POST)
     public ResponseEntity<Employee> Save(@RequestBody Employee employee) {
         return ResponseEntity.ok(employeeService.save(employee));
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<?> Delete(@PathVariable Long id) {
         employeeService.delete(id);
         return ResponseEntity.ok("Deleted");
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Employee> Update(@RequestBody Employee employeeDto, @PathVariable Long id) {
         return ResponseEntity.ok(employeeService.update(employeeDto, id));
     }
